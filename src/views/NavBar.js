459d730b9a1b959c3d5957d13f1f22cd021a440a
Hansel Omondi <ogopabhs@gmail.com>
2024-07-30 16:13:53 +0300

    Add: logout button to Navbar component

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -1,12 +1,16 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 const NavBar = () => {
+    const { logoutUser } = useContext(AuthContext);
     return (
         <div className='navbar'>
             <h2>Task Manager</h2>
             <div>
                 <Link to='/'>Home</Link>
                 <Link to='/create'>New Task</Link>
+                <button onClick={() => logoutUser()}>Logout</button>
             </div>
         </div>
     );
