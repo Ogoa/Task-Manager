64c0a3ec270cc66eb4c1ecfd1c7af98fc62da621
Hansel Omondi <ogopabhs@gmail.com>
2024-07-30 14:53:12 +0300

    Remove: unnecessary paths from api/urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path("token/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", views.RegisterView.as_view(), name="auth_register"),
-    # path("test/", views.testEndpoint, name="test_endpoint"),
-    # path("", views.getRoutes),
 ]
